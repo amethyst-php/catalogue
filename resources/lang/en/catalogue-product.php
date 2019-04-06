a9551054ead9a084e95b81dba71ad1672ab68164
railken <railken@protonmail.ch>
2019-04-06 23:52:41 +0200

    typo

---
+++ resources/lang/en/catalogue-product.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'name' => 'CatalogueProduct',
+    'description' => 'CatalogueProduct',
+    'attributes' => [
+        'name' => [
+            'label' => 'name',
+            'description' => 'Identify the record by a human-readable string'
+        ]
+    ]
+];
--- resources/lang/en/catalogue.php
+++ resources/lang/en/catalogue.php
@@ -16,4 +16,4 @@ return [
     'tabs' => [
         'products' => 'Products'
     ]
-]
+];
